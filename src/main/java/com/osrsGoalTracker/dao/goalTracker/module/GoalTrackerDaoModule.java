0e9789a313b99a13b0d370f64303a31389611ab0
eow <yaoeow@gmail.com>
2025-01-06 17:13:05 -0800

    fixing guice errors

--- src/main/java/com/osrsGoalTracker/dao/goalTracker/module/GoalTrackerDaoModule.java
+++ src/main/java/com/osrsGoalTracker/dao/goalTracker/module/GoalTrackerDaoModule.java
@@ -1,18 +1,29 @@
 package com.osrsGoalTracker.dao.goalTracker.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.osrsGoalTracker.dao.goalTracker.GoalTrackerDao;
 import com.osrsGoalTracker.dao.goalTracker.internal.ddb.DynamoGoalTrackerDao;
 
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
 /**
- * Module for the GoalTrackerDao.
+ * Guice module for GoalTrackerDao dependencies.
  */
 public class GoalTrackerDaoModule extends AbstractModule {
-    /**
-     * Configures the GoalTrackerDao module.
-     */
+
     @Override
     protected void configure() {
         bind(GoalTrackerDao.class).to(DynamoGoalTrackerDao.class);
+    }
+
+    @Provides
+    @Singleton
+    DynamoDbClient provideDynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.of(System.getenv("AWS_REGION")))
+                .build();
     }
 }
